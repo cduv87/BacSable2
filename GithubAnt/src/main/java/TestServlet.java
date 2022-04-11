e54a8cf555131a53e10c8b34e9b7d13d87199763
Antoine1989 <antoine-guillou@orange.fr>
2022-04-11 15:23:23 +0200

    test

---
+++ GithubAnt/src/main/java/TestServlet.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("")
+public class TestServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// On redirige le traitement à la JSP d'accueil
+		request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
